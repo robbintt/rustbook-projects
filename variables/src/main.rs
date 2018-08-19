b0c09f6fb7369a1d7686a8b1dae6f35c709bb6c0
Trent Robbins <robbintt@gmail.com>
2018-08-19 00:41:50 -0700

    Added shadowing same and different type

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,22 @@
 fn main() {
+
+
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+
+    // shadowing variables
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+
+    println!("The value of y is: {}", y);
+
+    // shadowing with a new type is fine
+    let spaces = "    ";
+    let spaces = spaces.len();
+
+    println!("There are {} spaces.", spaces);
 }
