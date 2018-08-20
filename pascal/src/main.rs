724ae417c442b5dd51c164f0e3af4fb16e6656ac
Trent Robbins <robbintt@gmail.com>
2018-08-20 14:29:02 -0700

    Simplified pascal_triangle Vec(Vec()) construction

--- pascal/src/main.rs
+++ pascal/src/main.rs
@@ -10,32 +10,36 @@ fn main() {
     let mut row = 1;
     let size = 10;
 
-    let mut pascal_triangle: Vec<Vec<isize>> = Vec::new();
+    // instead of ::new() lets try ::with_capacity() - since we know our expected capacity up front
+    let mut pascal_triangle: Vec<Vec<isize>> = Vec::with_capacity(size);
 
 
     pascal_triangle.push(vec![1]);
 
     while row <= size {
 
-        let mut this_row = Vec::new();
+        pascal_triangle.push(Vec::new());
 
-        // initialize this_row
-        // ideally i would toss this right on pascal_triangle and be able to directly access
-        // the second to last element of pascal_triangle as well.
-        this_row.push(1);
+        let pt_len = pascal_triangle.len();
+
+        // left element of row is 1 
+        &pascal_triangle[pt_len-1].push(1);
 
         // construct a new this_row from last_row's sums
         let mut i = 0;
         // if the vec has 5 elements, we want to grab up to indexes (3, 4) 
-        while i+2 <= pascal_triangle.last().unwrap().len() {
-            this_row.push(&pascal_triangle.last().unwrap()[i]+&pascal_triangle.last().unwrap()[i+1]);
+        while i+2 <= pascal_triangle[pt_len-2].len() {
+            // I had to move these to 2 lines because i wasn't able to access pascal_triangle as
+            // mutable inside of accessing pascal_triangle as immutable
+            // or maybe it was immutable inside mutable, i forget
+            let next = &pascal_triangle[pt_len-2][i]+&pascal_triangle[pt_len-2][i+1];
+            &pascal_triangle[pt_len-1].push(next);
             i = i+1;
         }
 
-        // finalize this_row
-        this_row.push(1);
+        // right element of row is 1 
+        &pascal_triangle[pt_len-1].push(1);
 
-        pascal_triangle.push(this_row);
         row = row + 1;
     }
 
@@ -43,6 +47,5 @@ fn main() {
     for row in pascal_triangle {
         println!("{:?}", row);
     }
-    //println!("{:?}", pascal_triangle);
 
 }
