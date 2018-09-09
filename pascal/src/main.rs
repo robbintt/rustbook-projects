2db46467b439792020fb71aef1d408204670d4a4
Trent Robbins <robbintt@gmail.com>
2018-09-08 21:30:29 -0700

    Rewrote the triangle as a function and solved a few ways, finally settled on an arbitrarily large int type.

--- pascal/src/main.rs
+++ pascal/src/main.rs
@@ -6,11 +6,103 @@
  *
  * Some other ideas:
  *  - write a recursive function to deliver the nth row of pascal's triangle
+ *
+ *
+ *  trent: i think i will write a write a function that calculates the nth row of pascal's 
+ *  triangle recursively and use that with map, and maybe use the actual formula to calculate 
+ *  the nth row non-recursively which is sort of trivial
  */
+
+extern crate num_bigint;
+extern crate ramp;
+
+use ramp::Int;
+
+use std::thread;
+use std::time::Duration;
 
 fn main() {
 
-    full_triangle();
+    // generate a full triangle and keep it all around
+    // full_triangle();
+    
+    // see biggest of each type from: https://www.reddit.com/r/rust/comments/6gxvs2/big_numbers_in_rust/
+    // println!("{}", std::u64::MAX);
+    // println!("{}", std::u128::MAX);
+
+    // int works up to 67
+    // bigint works up to 132
+    // ramp should work forever but is not a static type so need to use borrow
+    // gets slow around 2000 or so depending on your definition
+    let size = 10000; 
+
+    // generate a bunch of rows in place - don't keep the previous row
+    let mut current_triangle_row = Vec::new();
+    while current_triangle_row.len() < size {
+        current_triangle_row = build_next_row_pascal_triangle(current_triangle_row);
+        //thread::sleep(Duration::from_millis(100));
+        //println!("{:?}", current_triangle_row);
+        println!("{}", current_triangle_row.len());
+    }
+
+}
+
+/*
+fn get_nth_pascal_triangle_row (n: u64) -> Vec<u64> {
+
+    /* unravel the full_triangle function to have a recursive component
+     * atomic function will generate the next row from a given row
+     */
+
+}
+*/
+
+
+fn build_next_row_pascal_triangle (base_row: Vec<Int>) -> Vec<Int> {
+
+    /* needs bigint for triangles over 66 deep */
+
+    let mut next_row = Vec::new();
+
+
+    // if you want to use Int in the match statement you must instantiate them first
+    // it would be nice if match accepted an expression like this: { Int::from(0) }
+    // i no longer use them here though
+    let Int_1 = Int::from(1);
+
+    match base_row.len() {
+        0 => { 
+            next_row.push(Int::one());
+        }
+        1 => { 
+            next_row.push(Int::one());
+            next_row.push(Int::one());
+        },
+        // put the row builder here
+        _ => {
+            // left element of row is 1 
+            next_row.push(Int::one());
+
+            // construct a new this_row from last_row's sums
+            let mut i = 0;
+            // if the vec has 5 elements, we want to grab up to indexes (3, 4) 
+            while i+2 <= base_row.len() {
+                // I had to move these to 2 lines because i wasn't able to access pascal_triangle as
+                // mutable inside of accessing pascal_triangle as immutable
+                // or maybe it was immutable inside mutable, i forget
+                // 20180908 is this true when these vecs are not in the same parent vec?
+                // println!("{}", base_row[i]);
+                let nextval = &base_row[i]+&base_row[i+1];
+                next_row.push(nextval);
+                i = i+1;
+            }
+
+            // right element of row is 1 
+            next_row.push(Int::one());
+        }
+    }
+
+    next_row
 
 }
 
